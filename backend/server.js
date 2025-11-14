c28989f37f89635f0c1024ff9ae4638e225dd6af
ThanSitt <t3mith00@students.oamk.fi>
2025-11-14 17:44:23 +0200

    edit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello from Render!");
+});
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
